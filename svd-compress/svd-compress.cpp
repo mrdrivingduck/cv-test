539286e71b3088a2e9a5120ecc52cb997185af9f
mrdrivingduck <562655624@qq.com>
2019-12-06 13:14:11 +0800

    Refactor display

--- svd-compress/svd-compress.cpp
+++ svd-compress/svd-compress.cpp
@@ -10,21 +10,23 @@
 // ************************************************************************************
 
 #include <iostream>
+#include <string>
 #include <opencv2/opencv.hpp>
 
 using namespace cv;
 using std::cout;
 using std::endl;
+using std::string;
 
 //
 // For compressing
 //
 void SVD_Compress(const cv::Mat& inputImg, cv::Mat& outputImg, double compressRatio)
 {
-	cv::Mat tempt;
+	cv::Mat input;
 	cv::Mat U, W, V;
-	inputImg.convertTo(tempt, CV_32FC1);
-	cv::SVD::compute(tempt, W, U, V);
+	inputImg.convertTo(input, CV_32FC1);
+	cv::SVD::compute(input, W, U, V);
 
 	// W is a one-dimension vector which contains the singular values
 	// initialize a new 0-empty matrix of W
@@ -37,6 +39,8 @@ void SVD_Compress(const cv::Mat& inputImg, cv::Mat& outputImg, double compressRa
 		w.ptr<float>(i)[i] = W.ptr<float>(i)[0];
 	}
 
+	cout << W << endl;
+
 	// form the image again using the compressed matrix
 	cv::Mat result = U * w * V;
 	result.convertTo(outputImg, CV_8UC1);
@@ -44,15 +48,18 @@ void SVD_Compress(const cv::Mat& inputImg, cv::Mat& outputImg, double compressRa
 
 int main()
 {
+	string img("imgs/self.jpg");
 	// source image
-	cv::Mat scrX = imread("image.jpg", 0);
+	cv::Mat source = imread(img, 0);
+	cv::Mat origin = imread(img, 0);
 	// result image
 	cv::Mat result;
 
 	// compressing
-	SVD_Compress(scrX, result, 0.5);
+	SVD_Compress(source, result, 0.1);
 
-	cv::imshow("1", result);
+	cv::imshow("origin", origin);
+	cv::imshow("compressed", result);
 	waitKey(0);
 }
 
