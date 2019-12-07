2087ee6690414a63e764b296668f32a8454c65d4
mrdrivingduck <562655624@qq.com>
2019-12-07 17:23:18 +0800

    Part of latex

--- svd-compress/svd-compress.cpp
+++ svd-compress/svd-compress.cpp
@@ -23,16 +23,20 @@ using std::stringstream;
 //
 // For compressing
 //
-void SVD_Compress(const cv::Mat& inputImg, cv::Mat& outputImg, double compressRatio)
+void SVD_Compress(const cv::Mat& inputImg, cv::Mat& U, cv::Mat& W, cv::Mat& Vt)
 {
 	cv::Mat input;
-	cv::Mat U, W, V;
 	inputImg.convertTo(input, CV_32FC1);
-	cv::SVD::compute(input, W, U, V);
+	cv::SVD::compute(input, W, U, Vt);
 
+	cout << W << endl;
+}
+
+void display(cv::Mat& U, cv::Mat& W, cv::Mat& Vt, double compressRatio) {
 	// W is a one-dimension vector which contains the singular values
 	// initialize a new 0-empty matrix of W
 	cv::Mat w = Mat::zeros(Size(W.rows, W.rows), CV_32FC1);
+	cv::Mat output;
 
 	int len = compressRatio * W.rows;
 	for (int i = 0; i < len; i++) {
@@ -41,35 +45,36 @@ void SVD_Compress(const cv::Mat& inputImg, cv::Mat& outputImg, double compressRa
 		w.ptr<float>(i)[i] = W.ptr<float>(i)[0];
 	}
 
-	cout << W << endl;
-
 	// form the image again using the compressed matrix
-	cv::Mat result = U * w * V;
-	result.convertTo(outputImg, CV_8UC1);
+	cv::Mat result = U * w * Vt;
+	result.convertTo(output, CV_8UC1);
+
+	stringstream ss;
+	ss << compressRatio;
+	cv::imshow("compressed " + ss.str(), output);
+	waitKey(0);
 }
 
 int main()
 {
-	string img("imgs/self.jpg");
 	// source image
+	string img("imgs/duck.jpg");
 	cv::Mat source = imread(img, 0);
 	cv::Mat origin = imread(img, 0);
-	
+
+	cv::Mat U;
+	cv::Mat Vt;
+	cv::Mat W;
+	SVD_Compress(source, U, W, Vt);
+
 	// ratios
 	double ratios[] = { 0.9, 0.5, 0.3, 0.2, 0.15, 0.1, 0.05, 0.02 };
-
 	// compressing
 	for (int i = 0; i < sizeof(ratios) / sizeof(ratios[0]); i++) {
-		cv::Mat sourceCopy(source);
-		cv::Mat result;
-		SVD_Compress(sourceCopy, result, ratios[i]);
-
-		stringstream ss;
-		ss << ratios[i];
-		cv::imshow("compressed " + ss.str(), result);
-		waitKey(0);
+		display(U, W, Vt, ratios[i]);
 	}
 
+	// origin image
 	cv::imshow("origin", origin);
 	waitKey(0);
 }
