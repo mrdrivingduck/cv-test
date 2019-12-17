c179c396fe7375b0e39c7346301b238509e2e6ac
mrdrivingduck <562655624@qq.com>
2019-12-17 23:25:56 +0800

    Refactor TAB

--- svd-compress/svd-compress.cpp
+++ svd-compress/svd-compress.cpp
@@ -2,7 +2,7 @@
 //
 // The project of the course Matrix Theory
 //
-// Author: mrdrivingduck @Nanjing
+// Author: @mrdrivingduck
 //
 // ************************************************************************************
 
@@ -24,64 +24,64 @@ using std::ofstream;
 //
 void SVD_Compress(const cv::Mat& inputImg, cv::Mat& U, cv::Mat& W, cv::Mat& Vt)
 {
-	cv::Mat input;
-	inputImg.convertTo(input, CV_32FC1);
-	cv::SVD::compute(input, W, U, Vt);
+    cv::Mat input;
+    inputImg.convertTo(input, CV_32FC1);
+    cv::SVD::compute(input, W, U, Vt);
 
-	cout << W << endl;
+    cout << W << endl;
 }
 
 void display(cv::Mat& U, cv::Mat& W, cv::Mat& Vt, double compressRatio) {
-	// W is a one-dimension vector which contains the singular values
-	// initialize a new 0-empty matrix of W
-	cv::Mat w = Mat::zeros(Size(W.rows, W.rows), CV_32FC1);
-	cv::Mat output;
+    // W is a one-dimension vector which contains the singular values
+    // initialize a new 0-empty matrix of W
+    cv::Mat w = Mat::zeros(Size(W.rows, W.rows), CV_32FC1);
+    cv::Mat output;
 
-	int len = compressRatio * W.rows;
-	for (int i = 0; i < len; i++) {
-		// copy the diagonal element from W to w
-		// only the pre-length element will be copied
-		w.ptr<float>(i)[i] = W.ptr<float>(i)[0];
-	}
+    int len = compressRatio * W.rows;
+    for (int i = 0; i < len; i++) {
+        // copy the diagonal element from W to w
+        // only the pre-length element will be copied
+        w.ptr<float>(i)[i] = W.ptr<float>(i)[0];
+    }
 
-	// form the image again using the compressed matrix
-	cv::Mat result = U * w * Vt;
-	result.convertTo(output, CV_8UC1);
+    // form the image again using the compressed matrix
+    cv::Mat result = U * w * Vt;
+    result.convertTo(output, CV_8UC1);
 
-	stringstream ss;
-	ss << compressRatio;
-	cv::imshow("compressed " + ss.str(), output);
-	waitKey(0);
+    stringstream ss;
+    ss << compressRatio;
+    cv::imshow("compressed " + ss.str(), output);
+    waitKey(0);
 }
 
 int main()
 {
-	// source image
-	string img("imgs/duck.jpg");
-	cv::Mat source = imread(img, 0);
-	cv::Mat origin = imread(img, 0);
+    // source image
+    string img("imgs/duck.jpg");
+    cv::Mat source = imread(img, 0);
+    cv::Mat origin = imread(img, 0);
 
-	cv::Mat U;
-	cv::Mat Vt;
-	cv::Mat W;
-	SVD_Compress(source, U, W, Vt);
+    cv::Mat U;
+    cv::Mat Vt;
+    cv::Mat W;
+    SVD_Compress(source, U, W, Vt);
 
-	ofstream fout("SingularValues");
-	fout << "singular values" << endl;
-	for (int i = 0; i < W.rows; i++) {
-		fout << W.at<float>(i, 0) << endl;
-	}
-	fout.close();
+    ofstream fout("SingularValues");
+    fout << "singular values" << endl;
+    for (int i = 0; i < W.rows; i++) {
+        fout << W.at<float>(i, 0) << endl;
+    }
+    fout.close();
 
-	// ratios
-	double ratios[] = { 0.9, 0.5, 0.3, 0.2, 0.15, 0.1, 0.05, 0.02 };
-	// compressing
-	for (int i = 0; i < sizeof(ratios) / sizeof(ratios[0]); i++) {
-		display(U, W, Vt, ratios[i]);
-	}
+    // ratios
+    double ratios[] = { 0.9, 0.5, 0.3, 0.2, 0.15, 0.1, 0.05, 0.02 };
+    // compressing
+    for (int i = 0; i < sizeof(ratios) / sizeof(ratios[0]); i++) {
+        display(U, W, Vt, ratios[i]);
+    }
 
-	// origin image
-	cv::imshow("origin", origin);
-	waitKey(0);
+    // origin image
+    cv::imshow("origin", origin);
+    waitKey(0);
 }
 
